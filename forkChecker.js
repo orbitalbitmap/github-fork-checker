5731ceb20d3269013caca510b66bdca4903da301
orbitalbitmap <orbitalbitmap@gmail.com>
2020-07-29 20:01:29 -0400

    Added comments and adjusted the console.log statements a little bit.

--- forkChecker.js
+++ forkChecker.js
@@ -1,22 +1,27 @@
 const axios = require('axios')
 
 const getForks = async (studentList) => {
+  // calls the github's api repos route for the SummerOfCode organization's 'week-09-classwork-express-web' repo's forked information
   const { data } = await axios.get('https://api.github.com/repos/SummerOfCode2020/week-09-classwork-express-web/forks')
 
+  // reduces the data down to an array of usernames
   const whoForked = data.reduce((userArray, object) => {
     if (object.owner) userArray.push(object.owner.login)
     return userArray
   }, [])
 
+  // filters out the users from the 'studentList' variable that are not in the 'whoForked' variable
   let didNotFork = studentList.filter(student => !whoForked.includes(student))
 
-  console.log({ whoForked })
-  console.log({ didNotFork })
+  //logs to the console who has and hasn't forked the repo
+  console.log(whoForked.length ? { whoForked } : 'No one has forked the repo.')
+  console.log(didNotFork.lenght ? { didNotFork } : 'Everyone has forked the repo.')
 
 
   return data
 }
 
+// initializes the studentList
 const studentList = [
   'AL743-ni82dy',
   'alphaComara',
@@ -37,4 +42,5 @@ const studentList = [
   'zward2',
 ]
 
+// calls the function
 getForks(studentList)
